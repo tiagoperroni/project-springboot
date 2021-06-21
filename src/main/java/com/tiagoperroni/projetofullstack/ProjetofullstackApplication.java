e18141b01b03a12f05fb632cac6ec44b967f09fc
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-21 18:11:35 -0300

    Project created

---
+++ src/main/java/com/tiagoperroni/projetofullstack/ProjetofullstackApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.projetofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofullstackApplication.class, args);
+	}
+
+}
