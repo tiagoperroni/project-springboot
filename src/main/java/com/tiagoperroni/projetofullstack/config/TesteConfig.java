dba320ea6678727772139a27f15d42de8353508c
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-23 09:47:42 -0300

    Payment one-to-one association

--- src/main/java/com/tiagoperroni/projetofullstack/config/TesteConfig.java
+++ src/main/java/com/tiagoperroni/projetofullstack/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.tiagoperroni.projetofullstack.entities.Category;
 import com.tiagoperroni.projetofullstack.entities.Order;
 import com.tiagoperroni.projetofullstack.entities.OrderItem;
+import com.tiagoperroni.projetofullstack.entities.Payment;
 import com.tiagoperroni.projetofullstack.entities.Product;
 import com.tiagoperroni.projetofullstack.entities.User;
 import com.tiagoperroni.projetofullstack.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
