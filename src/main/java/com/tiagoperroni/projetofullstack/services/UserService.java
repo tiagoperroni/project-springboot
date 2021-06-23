1ae68af9c3050a25a2ee1e731d9f9e2c85dd0a62
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-23 12:05:57 -0300

    Exception handling - delete

--- src/main/java/com/tiagoperroni/projetofullstack/services/UserService.java
+++ src/main/java/com/tiagoperroni/projetofullstack/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.tiagoperroni.projetofullstack.entities.User;
 import com.tiagoperroni.projetofullstack.repositories.UserRepository;
+import com.tiagoperroni.projetofullstack.services.exceptions.DatabaseException;
 import com.tiagoperroni.projetofullstack.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
